8e987ca01c635521666eacd38bb51285b50b0fdf
Nicolas Betancur <nicobetancurochoa@gmail.com>
2024-02-22 17:10:35 -0500

    Almost done

--- Tutorial1/pages/urls.py
+++ Tutorial1/pages/urls.py
@@ -9,7 +9,8 @@ from .views import *
 urlpatterns = [ 
 
     path("", HomePage.as_view(), name='home'), 
-
-    path("about/", AboutPageView.as_view(), name='about') 
-
+    path("about/", AboutPageView.as_view(), name='about') ,
+    path("contact/",ContactPage.as_view(), name='contact'),
+    path('products/', ProductIndexView.as_view(), name='index'), 
+    path('products/<str:id>', ProductShowView.as_view(), name='show'), 
 ]   
--- Tutorial1/pages/views.py
+++ Tutorial1/pages/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render 
-
+from django.views import View 
 from django.http import HttpResponse 
-
 from django.views.generic import TemplateView 
 
  
@@ -13,9 +12,17 @@ class HomePage(TemplateView):
     template_name = 'pages/home.html' 
 
 
+
+class ContactPage(TemplateView): 
+
+    template_name = 'pages/contact.html' 
+
+
 class AboutPageView(TemplateView): 
 
     template_name = 'pages/about.html' 
+
+
     
     def get_context_data(self, **kwargs): 
 
@@ -36,3 +43,65 @@ class AboutPageView(TemplateView):
  
 
         return context 
+    
+class Product: 
+
+    products = [ 
+
+        {"id":"1", "name":"TV", "description":"Best TV"}, 
+
+        {"id":"2", "name":"iPhone", "description":"Best iPhone"}, 
+
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"}, 
+
+        {"id":"4", "name":"Glasses", "description":"Best Glasses"} 
+
+    ] 
+
+ 
+
+class ProductIndexView(View): 
+
+    template_name = 'products/index.html' 
+
+ 
+
+    def get(self, request): 
+
+        viewData = {} 
+
+        viewData["title"] = "Products - Online Store" 
+
+        viewData["subtitle"] =  "List of products" 
+
+        viewData["products"] = Product.products 
+
+ 
+
+        return render(request, self.template_name, viewData) 
+
+ 
+
+class ProductShowView(View): 
+
+    template_name = 'products/show.html' 
+
+ 
+
+ 
+
+    def get(self, request, id): 
+
+        viewData = {} 
+
+        product = Product.products[int(id)-1] 
+
+        viewData["title"] = product["name"] + " - Online Store" 
+
+        viewData["subtitle"] =  product["name"] + " - Product information" 
+
+        viewData["product"] = product 
+
+ 
+
+        return render(request, self.template_name, viewData)
