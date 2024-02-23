2e71824e7a77f84bdd07d9f30bb253e4a03ca3a8
Nicolas Betancur <nicobetancurochoa@gmail.com>
2024-02-22 20:28:23 -0500

    Last commit

--- Tutorial1/pages/models.py
+++ Tutorial1/pages/models.py
@@ -10,3 +10,11 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True) 
 
     updated_at = models.DateTimeField(auto_now=True) 
+
+ 
+
+class Comment(models.Model): 
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE) 
+
+    description = models.TextField() 
--- Tutorial1/pages/views.py
+++ Tutorial1/pages/views.py
@@ -1,9 +1,11 @@
 from django.views import View 
 from django.http import HttpResponse 
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView, ListView
 from django.http import HttpResponseRedirect
 from django import forms 
-from django.shortcuts import render, redirect 
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product 
+from django.core.exceptions import ValidationError
 
 # Create your views here. 
 
@@ -44,23 +46,8 @@ class AboutPageView(TemplateView):
 
         return context 
     
-class Product: 
-
-    products = [ 
-
-        {"id":"1", "name":"TV", "description":"Best TV","price":"$1000.99"}, 
-
-        {"id":"2", "name":"iPhone", "description":"Best iPhone","price":"$1200.99"}, 
-
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast","price":"$40.99"}, 
-
-        {"id":"4", "name":"Glasses", "description":"Best Glasses","price":"$12.99"} 
-
-
-    ] 
 
  
-
 class ProductIndexView(View): 
 
     template_name = 'products/index.html' 
@@ -75,13 +62,23 @@ class ProductIndexView(View):
 
         viewData["subtitle"] =  "List of products" 
 
-        viewData["products"] = Product.products 
+        viewData["products"] = Product.objects.all() 
 
-        
-
-       
+ 
 
         return render(request, self.template_name, viewData) 
+
+ 
+
+ 
+
+class ProductShowView(View): 
+
+    template_name = 'products/show.html' 
+
+ 
+
+ 
 
  
 
@@ -94,13 +91,16 @@ class ProductShowView(View):
     def get(self, request, id): 
         
         try:
-            product = Product.products[int(id)-1]
+            product_id = int(id) 
+            if product_id < 1: 
+                raise ValueError("Product id must be 1 or greater") 
+            product = get_object_or_404(Product, pk=product_id) 
         except IndexError:
             return HttpResponseRedirect('/')
 
         viewData = {} 
 
-        product = Product.products[int(id)-1] 
+        product = get_object_or_404(Product, pk=product_id) 
 
         viewData["title"] = product["name"] + " - Online Store" 
 
@@ -113,14 +113,53 @@ class ProductShowView(View):
  
 
         return render(request, self.template_name, viewData)
-    
-class ProductForm(forms.Form): 
 
-    name = forms.CharField(required=True) 
+class ProductListView(ListView): 
 
-    price = forms.FloatField(required=True) 
+    model = Product 
+
+    template_name = 'product_list.html' 
+
+    context_object_name = 'products'   
 
  
+
+    def get_context_data(self, **kwargs): 
+
+        context = super().get_context_data(**kwargs) 
+
+        context['title'] = 'Products - Online Store' 
+
+        context['subtitle'] = 'List of products' 
+
+        return context  
+
+
+
+
+### forms # ---------------------------------------------    
+
+ 
+
+class ProductForm(forms.ModelForm): 
+
+    class Meta: 
+
+        model = Product 
+
+        fields = ['name', 'price'] 
+
+ 
+
+    def clean_price(self): 
+
+        price = self.cleaned_data.get('price') 
+
+        if price is not None and price <= 0: 
+
+            raise ValidationError('Price must be greater than zero.') 
+
+        return price 
 
  
 
@@ -150,9 +189,9 @@ class ProductCreateView(View):
 
         if form.is_valid(): 
 
-             
+            form.save() 
 
-            return redirect(form)  
+            return redirect('product-created')  
 
         else: 
 
@@ -162,4 +201,4 @@ class ProductCreateView(View):
 
             viewData["form"] = form 
 
-            return render(request, self.template_name, viewData)
+            return render(request, self.template_name, viewData) 
