6b85233d142d79f4fe53612df2468075e1047e82
Nicolas Betancur <nicobetancurochoa@gmail.com>
2024-02-29 17:04:05 -0500

    tutorial 08 a y b

--- Tutorial1/pages/apps.py
+++ Tutorial1/pages/apps.py
@@ -1,6 +1,19 @@
-from django.apps import AppConfig
+from django.apps import AppConfig 
 
+from django.utils.module_loading import import_string 
 
-class PagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pages'
+from django.conf import settings 
+
+ 
+
+class PagesConfig(AppConfig): 
+
+    default_auto_field = 'django.db.models.BigAutoField' 
+
+    name = 'pages' 
+
+ 
+
+    def ready(self): 
+
+        ImageStorageClass = import_string(settings.IMAGE_STORAGE_CLASS) 
